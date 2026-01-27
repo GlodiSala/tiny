85a2ef61939b727ea00410686bf303e2c4249bfe
Glodi Sala Mangituka <119762719+GlodiSala@users.noreply.github.com>
2026-01-26 21:44:26 -0500

    Refactor BranchUnit inputs and logic

--- Modules/BranchUnit.v
+++ Modules/BranchUnit.v
@@ -1,38 +1,39 @@
 `timescale 1ns / 1ps
 
 module BranchUnit(
-    input [3:0] branch_type,
-    input [15:0] branch_offset,      // Déjà extrait par ControlUnit
-    input[1:0] stored_flags,
-    input branch_enable,
+    input [3:0]  branch_type,
+    input [15:0] branch_offset,
+    input [3:0]  stored_flags,    // REMIS À 4 BITS
+    input        branch_enable,
     input [15:0] pc_current,
     output reg [15:0] pc_next
 );
 
-// Répète le bit de signe (offset[11]) sur 4 bits pour passer de 12→16 bits
-// Exemple : 0xFFA (−6) devient 0xFFFA (toujours −6)
+    localparam JMP  = 4'b1001;
+    localparam BRZ  = 4'b1010;
+    localparam BRNZ = 4'b1011;
+    localparam BRNS = 4'b1100;
 
-localparam JMP  = 4'b1001;  // jump
-localparam BRZ = 4'b1010;  // branch if zero
-localparam BRNZ = 4'b1011; // Branch if not zero	
-localparam BRNS = 4'b1100; // BRNS : branch if not overflow
-
-reg branch;    
-always @(*) begin
-    case (branch_type)
-        JMP : branch = 1;
-        BRZ : branch = stored_flags[0];
-        BRNZ : branch = ~stored_flags[0];
-        BRNS : branch = ~stored_flags[1];
-        default : branch = 0;
-    endcase
-end
-always @(*) begin // logique combinatoire
-    if (branch_enable && branch) begin
-        pc_next = pc_current + branch_offset;  // Relative jump
-    end else begin
-        pc_next = pc_current + 1;              // Next instruction
+    reg branch;    
+    always @(*) begin
+        case (branch_type)
+            JMP  : branch = 1;
+            BRZ  : branch = stored_flags[0];  // Zero flag
+            BRNZ : branch = ~stored_flags[0];
+            BRNS : branch = ~stored_flags[1]; // Overflow flag
+            default : branch = 0;
+        endcase
     end
-end
-wire [1:0] _unused_flags = stored_flags[3:2];
+
+    always @(*) begin
+        if (branch_enable && branch) begin
+            pc_next = pc_current + branch_offset;
+        end else begin
+            pc_next = pc_current + 1;
+        end
+    end
+
+    // ON CONSOMME LES BITS 2 et 3 ICI POUR LE LINTER
+    wire [1:0] _unused_flags = stored_flags[3:2];
+
 endmodule
